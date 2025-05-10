ff9a3d817c64962033a5aa1b97fc9a2d3bb1b339
Pito Desri Pauzi <pitokfauzi@gmail.com>
2025-05-10 17:40:55 +0700

    feat: add validation for barang creation and update routes

--- backend/src/routes/barang.routes.ts
+++ backend/src/routes/barang.routes.ts
@@ -1,12 +1,14 @@
-import{ Router } from 'express';
+import { Router } from 'express';
 import { createBarangController, deleteBarangController, getAllBarangController, getBarangByIdController, updateBarangController } from '../controllers/barang.controller';
+import { barangValidator } from '../validators/barang.validator';
+import { handleValidationErrors } from '../middlewares/handle_validation_errors';
 
 export const barangRouter = Router()
 
 barangRouter.get('/', getAllBarangController)
 barangRouter.get('/:id', getBarangByIdController)
-barangRouter.post('/', createBarangController)
-barangRouter.put('/:id', updateBarangController)
+barangRouter.post('/', barangValidator, handleValidationErrors, createBarangController)
+barangRouter.put('/:id', barangValidator, handleValidationErrors, updateBarangController)
 barangRouter.delete('/:id', deleteBarangController)
 
 export default barangRouter
